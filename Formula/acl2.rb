9ccf41880cc309530d792c120856910c66a43313
TANIGUCHI Masaya <m@docs.casa>
2021-01-22 18:05:07 +0900

    Add acl2

---
+++ Formula/acl2.rb
@@ -0,0 +1,42 @@
+class Acl2 < Formula
+  desc "Logic and programming language in which you can model computer systems"
+  homepage "https://www.cs.utexas.edu/users/moore/acl2/index.html"
+  url "https://github.com/acl2/acl2/archive/8.3.tar.gz"
+  sha256 "45eedddb36b2eff889f0dba2b96fc7a9b1cf23992fcfdf909bc179f116f2c5ea"
+  license "BSD-3-Clause"
+  revision 4
+
+  depends_on "sbcl"
+
+  def install
+    system "make",
+           "LISP=#{HOMEBREW_PREFIX}/bin/sbcl",
+           "ACL2=#{buildpath}/saved_acl2",
+           "USE_QUICKLISP=0",
+           "all", "basic"
+    system "make",
+           "LISP=#{HOMEBREW_PREFIX}/bin/sbcl",
+           "ACL2_PAR=p",
+           "ACL2=#{buildpath}/saved_acl2p",
+           "USE_QUICKLISP=0",
+           "all", "basic"
+    libexec.install Dir["*"]
+
+    (bin/"acl2").write <<~EOF
+      #!/bin/sh
+      export ACL2_SYSTEM_BOOKS='#{libexec}/books'
+      #{Formula["sbcl"].opt_bin}/sbcl --core '#{libexec}/saved_acl2.core' --userinit /dev/null --eval '(acl2::sbcl-restart)'
+    EOF
+    (bin/"acl2p").write <<~EOF
+      #!/bin/sh
+      export ACL2_SYSTEM_BOOKS='#{libexec}/books'
+      #{Formula["sbcl"].opt_bin}/sbcl --core '#{libexec}/saved_acl2p.core' --userinit /dev/null --eval '(acl2::sbcl-restart)'
+    EOF
+  end
+
+  test do
+    (testpath/"simple.lisp").write "(+ 2 2)"
+    output = shell_output("#{bin}/acl2 < #{testpath}/simple.lisp | grep 'ACL2 !>'")
+    assert_equal "ACL2 !>4\nACL2 !>Bye.", output.strip
+  end
+end
