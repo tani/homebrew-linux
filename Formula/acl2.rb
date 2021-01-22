0f92ee6bc341bc237110e9749ae639bb28ac98ff
TANIGUCHI Masaya <m@docs.casa>
2021-01-22 18:40:22 +0900

    Remove revision

--- Formula/acl2.rb
+++ Formula/acl2.rb
@@ -4,7 +4,6 @@ class Acl2 < Formula
   url "https://github.com/acl2/acl2/archive/8.3.tar.gz"
   sha256 "45eedddb36b2eff889f0dba2b96fc7a9b1cf23992fcfdf909bc179f116f2c5ea"
   license "BSD-3-Clause"
-  revision 4
 
   depends_on "sbcl"
 
