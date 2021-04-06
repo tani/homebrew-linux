9778708a7593347e2ce656db92a905cc2a1db908
TANIGUCHI Masaya <m@docs.casa>
2021-04-06 13:54:26 +0900

    Isabelle with Emacs support

--- Formula/isabelle.rb
+++ Formula/isabelle.rb
@@ -1,9 +1,7 @@
 class Isabelle < Formula
-  desc "Isabelle is a generic proof assistant."
+  desc "Isabelle is a generic proof assistant with Emacs support."
   homepage "https://isabelle.in.tum.de"
-  url "https://mirror.clarkson.edu/isabelle/dist/Isabelle2020_linux.tar.gz"
-  version "2020"
-  sha256 "633aff864d6647bd175cf831e7513e3fd0cd06beacbf29a5c6c66d4de1522bae"
+  head "git://github.com/m-fleury/isabelle-release", branch: "Isabelle2021-more-vscode"
   license "BSD-3-Clause"
 
   def install
