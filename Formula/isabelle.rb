2549eabcceb51c5c6599f18a44214c8a550a5d04
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-09-03 14:16:39 +0900

    Add build commands

--- Formula/isabelle.rb
+++ Formula/isabelle.rb
@@ -1,11 +1,14 @@
 class Isabelle < Formula
   desc "Isabelle is a generic proof assistant with Emacs support."
   homepage "https://isabelle.in.tum.de"
-  head "git://github.com/m-fleury/isabelle-release", branch: "Isabelle2021-more-vscode"
+  head "git://github.com/m-fleury/isabelle-emacs", branch: "Isabelle2021-more-vscode"
   license "BSD-3-Clause"
 
   def install
     libexec.install Dir["*"]
+    system [libexec/"bin"/"isabelle", "components", "-I"]
+    system [libexec/"bin"/"isabelle", "components", "-a"]
+    system [libexec/"bin"/"isabelle", "build", "-b", "HOL"]
     bin.install_symlink libexec/"bin"/"isabelle"
   end
 
