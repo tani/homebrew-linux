38b57e86ebab9957ba4083cfcd2c817ce722bf87
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-04-07 11:35:16 +0900

    chore: refactor neovim formula file and remove unnecessary code
    - Remove unnecessary code related to updating formula
    - Change `url` to `head` in neovim formula file
    
    Signed-off-by: TANIGUHCI Masaya <ta2gch@gmail.com>

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -1,8 +1,7 @@
 class Neovim < Formula
   desc "hyperextensible Vim-based text editor"
   homepage "https://neovim.io"
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
-  sha256 "d18fdb2e76adfc3e438915f7b4b226041a8a2bfcd2d158304e441b4acfbb9b24"
+  head "https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz", using: :curl
   license "Apache"
 
   def install
