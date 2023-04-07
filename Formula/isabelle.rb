e943486b38def323066a613054e78082fd640ccd
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-04-07 11:43:36 +0900

    chore: update head URL in Isabelle formula
    - Add new file `Formula/.isabelle.rb.swp`
    - Change the `head` URL in `Formula/isabelle.rb`
    
    Signed-off-by: TANIGUHCI Masaya <ta2gch@gmail.com>

--- Formula/isabelle.rb
+++ Formula/isabelle.rb
@@ -1,7 +1,7 @@
 class Isabelle < Formula
   desc "Isabelle is a generic proof assistant with Emacs support."
   homepage "https://isabelle.in.tum.de"
-  head "git://github.com/m-fleury/isabelle-emacs", branch: "Isabelle2021-more-vscode"
+  head "https://github.com/m-fleury/isabelle-emacs", branch: "Isabelle2021-more-vscode"
   license "BSD-3-Clause"
 
   def install
