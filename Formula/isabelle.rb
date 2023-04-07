8cc311ac64096692b4047564a6b468599ec17efc
TANIGUHCI Masaya <ta2gch@gmail.com>
2023-04-07 12:21:07 +0900

    refactor: refactor system calls for efficiency
    - Change `head` specification to use git protocol
    - Remove unnecessary brackets in `system` calls
    - Shorten `system` calls by removing the path to the executable
    
    Signed-off-by: TANIGUHCI Masaya <ta2gch@gmail.com>

--- Formula/isabelle.rb
+++ Formula/isabelle.rb
@@ -1,14 +1,14 @@
 class Isabelle < Formula
   desc "Isabelle is a generic proof assistant with Emacs support."
   homepage "https://isabelle.in.tum.de"
-  head "https://github.com/m-fleury/isabelle-emacs", branch: "Isabelle2021-more-vscode"
+  head "https://github.com/m-fleury/isabelle-emacs", using: :git
   license "BSD-3-Clause"
 
   def install
     libexec.install Dir["*"]
-    system [libexec/"bin"/"isabelle", "components", "-I"]
-    system [libexec/"bin"/"isabelle", "components", "-a"]
-    system [libexec/"bin"/"isabelle", "build", "-b", "HOL"]
+    system libexec/"bin"/"isabelle", "components", "-I"
+    system libexec/"bin"/"isabelle", "components", "-a"
+    system libexec/"bin"/"isabelle", "build", "-b", "HOL"
     bin.install_symlink libexec/"bin"/"isabelle"
   end
 
